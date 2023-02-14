bdf076ff3f28beca4fea48fe268b9fd67044ae34
unknown <mairan.cavalheiro@gmail.com>
2023-02-14 18:46:32 -0300

    atualizacao geral

--- assets/script/contato.js
+++ assets/script/contato.js
@@ -1,66 +1,121 @@
-const form = document.getElementById("form");
-const idnome = document.getElementById("idnome");
-const idemail = document.getElementById("idemail");
-const idtelefone = document.getElementById("idtelefone");
-
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  checkInputs();
-});
-
-function checkInputs() {
-  const idnomeValue = idnome.value;
-  const idemailValue = idemail.value;
-  const idtelefoneValue = idetelefone.value;
-
-  if (idnomeValue == null) {
-    setErrorFor(idnome, "O nome de usuário é obrigatório.");
-  } else {
-    setSuccessFor(idnome);
-  }
-
-  if (idemailValue == null) {
-    setErrorFor(idemail, "O email é obrigatório.");
-  } else if (!checkidemail(idemailValue)) {
-    setErrorFor(idemail, "Por favor, insira um e-mail válido.");
-  } else {
-    setSuccessFor(idemail);
-  }
-
-  if (idtelefoneValue == null) {
-    setErrorFor(idtelefone, "O telefone é obrigatório.");
-  } else {
-    setSuccessFor(idtelefone);
-  }
-
-  const formControls = form.querySelectorAll(".form-control");
-
-  const formIsValid = [...formControls].every((formControl) => {
-    return formControl.className === "form-control success";
+// ---------- FUNÇÕES GERAIS -------------- //
+function togglePopup(input, label) {
+  // Mostrar popup de campo obrigatório
+  input.addEventListener("focus", () => {
+    label.classList.add("required-popup");
   });
 
-  if (formIsValid) {
-    console.log("Dados enviados com sucesso!");
+  // Ocultar popup de campo obrigatório
+  input.addEventListener("blur", () => {
+    label.classList.remove("required-popup");
+  });
+}
+
+function estilizarInputCorreto(input, helper) {
+  helper.classList.remove("visible");
+  input.classList.remove("error");
+  input.classList.add("correct");
+  limparCadastro();
+}
+
+function estilizarInputIncorreto(input, helper) {
+  helper.classList.add("visible");
+  input.classList.add("error");
+  input.classList.remove("correct");
+  limparCadastro();
+}
+
+function limparCadastro() {
+    inputusername.value = '';
+    inputEmail.value = '';
+    inputIdade.value = '';
+    inputSenha.value = '';
+    inputConfirmarSenha.value = '';
+    input.classList.remove("error");
+    input.classList.remove("correct");
+    // input.classList.add("correct");
+    // input.classList.add("error");
+
+}
+
+
+// ---------- VALIDAÇÃO USERNAME ---------- //
+let usernameInput = document.getElementById("username");
+let usernameLabel = document.querySelector('label[for="username"]');
+let usernameHelper = document.getElementById("username-helper");
+
+togglePopup(usernameInput, usernameLabel)
+
+// Validar valor do input
+usernameInput.addEventListener("change", (e)=> {
+  let valor = e.target.value
+
+  if(valor.length < 3){
+    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    usernameHelper.innerText = "Seu username precisa ter 3 ou mais caracteres";
+    estilizarInputIncorreto(usernameInput, usernameHelper)
+    inputsCorretos.username = false;
+  } else {
+    // Adicionar estilos dinâmicos se o valor estiver correto
+    estilizarInputCorreto(usernameInput, usernameHelper);
+    inputsCorretos.username = true;
   }
+})
+
+// ---------- VALIDAÇÃO EMAIL ---------- //
+let emailInput = document.getElementById("email");
+let emailLabel = document.querySelector('label[for="email"]');
+let emailHelper = document.getElementById("email-helper");
+
+togglePopup(emailInput, emailLabel)
+
+// Validar valor do input
+emailInput.addEventListener("change", (e)=> {
+  let valor = e.target.value
+
+  if(valor.includes("@") && valor.includes(".com")){
+    // Adicionar estilos dinâmicos se o valor estiver correto
+    estilizarInputCorreto(emailInput, emailHelper);
+    inputsCorretos.email = true;
+  } else {
+    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    emailHelper.innerText = "Precisa inserir um email válido";
+    estilizarInputIncorreto(emailInput, emailHelper);
+    inputsCorretos.email = false;
+  }
+})
+
+// ---------- VALIDAÇÃO TELEFONE ---------- //
+let telefoneInput = document.getElementById("telefone");
+let telefoneLabel = document.querySelector('label[for="telefone"]');
+let telefoneHelper = document.getElementById("telefone-helper");
+
+togglePopup(telefoneInput, telefoneLabel)
+
+
+
+
+// ---- Habilitar Envio de Formulário ---- //
+
+const submitButton = document.getElementById('submit');
+limparCadastro();
+const inputsCorretos = {
+  username: true,
+  email: true,
+  telefone: true,
+  mensagem: true
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
 
-  // Adiciona a mensagem de erro
-  small.innerText = message;
+  const values = Object.values(inputsCorretos)
 
-  // Adiciona a classe de erro
-  formControl.className = "form-control error";
-}
+  const anyFalse = values.filter((value) => value === false)
 
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-
-  // Adicionar a classe de sucesso
-  formControl.className = "form-control success";
-}
-
+  if(!anyFalse.length) {
+    alert('Formulário enviado com sucesso!')
+  } else {
+    alert('Preencha todos os campos!')
+  }
+})
